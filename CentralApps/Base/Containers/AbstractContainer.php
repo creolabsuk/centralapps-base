82bf2116b3168896c30323b0522aeddbe3a1ee25
Michael Peacock <mkpeacock@gmail.com>
2013-06-16 10:55:29 +0100

    added the request to the container

--- CentralApps/Base/Containers/AbstractContainer.php
+++ CentralApps/Base/Containers/AbstractContainer.php
@@ -3,6 +3,24 @@ namespace CentralApps\Base\Containers;
 
 abstract class AbstractContainer extends \Pimple
 {
+
+	public function __construct (array $values = array())
+	{
+		parent::__construct($values);
+		$this->addRequestToContainer();
+	}
+
+	protected function addRequestToContainer()
+	{
+		$request = array();
+        $request['get'] = $_GET;
+        $request['post'] = $_POST;
+        $request['session'] = $_SESSION;
+        $request['cookies'] = $_COOKIE;
+        $request['server'] = $_SERVER;
+        $this['request'] = $request;
+	}
+
 	/**
 	 * Get a setting value which is deeply nested in the settings array, based on a structured array as the key
 	 * @param array $nested_key an array indicating the structure and depth of the setting in the settings array
@@ -10,13 +28,25 @@ abstract class AbstractContainer extends \Pimple
 	 * @param array $partially_processed_array if we are recursively looping, this will contain a subset of the settings array
 	 * @return mixed
 	 */
-	public function getSettingFromNestedKey($nested_key = array(), $settings_key = 'settings', $partially_processed_array=null)
+	public function getSettingFromNestedKey($nested_key = array(), $settings_key = 'settings')
 	{
-		$partially_processed_array = (is_null($partially_processed_array)) ? $this[$settings_key] : $partially_processed_array;
+		array_unshift($nested_key, $settings_key);
+		return $this->getFromNestedKey($nested_key);
+	}
+
+	/**
+	 * Get a value which is deeply nested in the container, based on a structured array as the key
+	 * @param array $nested_key an array indicating the structure and depth of the setting in the settings array
+	 * @param array $partially_processed_array if we are recursively looping, this will contain a subset of the settings array
+	 * @return mixed
+	 */
+	public function getFromNestedKey($nested_key=array(), $partially_processed_array=array())
+	{
+		$partially_processed_array = (is_null($partially_processed_array)) ? $this : $partially_processed_array;
 		if (count($nested_key) > 1) {
 			$current_key = array_shift($nested_key);
 			$partially_processed_array = $partially_processed_array[$current_key];
-			return $this->getSettingFromNestedKey($nested_key, $settings_key, $partially_processed_array);
+			return $this->getFromNestedKey($nested_key, $partially_processed_array);
 		} else {
 			return $partially_processed_array[array_shift($nested_key)];
 		}
