b7fa12e2ae6bec18d9cc44510c1fd1d89efc2cc0
Michael Peacock <mkpeacock@gmail.com>
2013-07-21 13:49:00 +0100

    Debug trait

---
+++ CentralApps/Base/DebugTrait.php
@@ -0,0 +1,22 @@
+<?php
+namespace CentralApps\Base;
+
+trait DebugTrait
+{
+	public function debug()
+	{
+		$this->removeContainer();
+		return print_r($this, true);
+	}
+
+	public function removeContainer()
+	{
+		$this->container = '[== CONTAINER PLACEHOLDER ==]';
+		foreach ($this as $key => $value) {
+			if (is_object($value) && array_key_exists('CentralApps\Base\DebugTrait', class_uses($value))) {
+				$value->removeContainer();
+				$this->$key = $value;
+			}
+		}
+	}
+}
