59cb4395a1620cc0ade486e0efa3c7cb0adcf339
Michael Peacock <mkpeacock@gmail.com>
2014-06-08 22:28:31 +0100

    removed some surplus stuff

--- bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-$namespaces = array(
-    'CentralApps\\Base' => __DIR__ .'/'
-);
-
-$loader = new \Symfony\Component\ClassLoader\UniversalClassLoader();
-$loader->register();
-$loader->registerNamespaces($namespaces);
