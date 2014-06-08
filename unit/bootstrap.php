017b1387277e1f05b51916d698995af280a8fb65
Michael Peacock <mkpeacock@gmail.com>
2014-06-08 22:33:33 +0100

    unit test bootstrap fix

--- unit/bootstrap.php
+++ unit/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
-require_once(__DIR__.'/../vendor/autoload.php');
+require_once(__DIR__.'/../../vendor/autoload.php');
 require_once(__DIR__.'/splClassLoader.php');
 $classLoader = new SplClassLoader('CentralApps', __DIR__.'/../');
 $classLoader->register();
