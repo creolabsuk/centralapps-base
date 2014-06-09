3eb858c9150e135d19a5624a9ec5a09a0ce7c385
Michael Peacock <mkpeacock@gmail.com>
2014-06-09 15:40:33 +0100

    Fixes

--- CentralApps/Base/ServiceProviders/Routing/AuthenticationRouteFilter.php
+++ CentralApps/Base/ServiceProviders/Routing/AuthenticationRouteFilter.php
@@ -3,24 +3,25 @@ namespace CentralApps\Base\ServiceProviders\Routing;
 
 class AuthenticationRouteFilter implements RouteFilter
 {
+    protected $container = [];
     protected $currentUser = null;
     protected $loggedInKey = 'logged_in';
     protected $notLoggedInException = '\Exception';
+
+    public function __construct($container)
+    {
+        $this->container = $container;
+    }
 
     public function setLoggedInException($exception_class)
     {
         $this->notLoggedInException = $exception_class;
     }
 
-    public function setCurrentUser($user)
-    {
-        $this->currentUser = $user;
-    }
-
     public function filterRoute(array $route)
     {
         if (isset($route[$this->loggedInKey]) && true == $route[$this->loggedInKey]) {
-            if (is_null($this->currentUser)) {
+            if (is_null($this->container['current_user'])) {
                 $exception_class = $this->notLoggedInException;
                 throw new $exception_class('You need to be logged in to view this page');
             }
