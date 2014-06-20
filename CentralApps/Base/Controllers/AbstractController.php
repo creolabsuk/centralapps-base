2656527ec6e02cb01ca5491f23a580892af6b161
Michael Peacock <mkpeacock@gmail.com>
2014-06-20 10:25:10 +0100

    Update AbstractController.php

--- CentralApps/Base/Controllers/AbstractController.php
+++ CentralApps/Base/Controllers/AbstractController.php
@@ -4,6 +4,11 @@ namespace CentralApps\Base\Controllers;
 abstract class AbstractController
 {
     protected $container = null;
+    protected $model;
+    protected $formHelper = null;
+    protected $formHelperPrep = null;
+    protected $request;
+    protected $requestArray = null;
 
     public function __construct(\CentralApps\Base\Containers\Container $container)
     {
@@ -39,5 +44,67 @@ abstract class AbstractController
 
         // ideally, view->render should return a symfony HTTP response object or equivalent
         return $view->render($model, $model_name);
+    }
+    
+    protected function populateRequest()
+    {
+        $this->request = \Symfony\Component\HttpFoundation\Request::createFromGlobals();
+    }
+
+    protected function get($request_key)
+    {
+        if (is_null($this->requestArray)) {
+            $this->getRequestArrayToValidate();
+        }
+
+        return isset($this->requestArray[$request_key]) ? $this->requestArray[$request_key] : '';
+    }
+
+    protected function getRequestArrayToValidate()
+    {
+        if (is_null($this->requestArray)) {
+            $this->requestArray = array_merge($this->request->request->all(), $this->request->files->all());
+        }
+
+        return $this->requestArray;
+    }
+
+    protected function getRequest()
+    {
+        return new \Symfony\Component\HttpFoundation\ParameterBag($this->getRequestArrayToValidate());
+    }
+
+    protected function validate($rules, $callback_method, $callback_params = array())
+    {
+        $data = $this->getRequestArrayToValidate();
+        $violations = $this->container['validation_rules'][$rules]->getViolations($data);
+        if (count($violations) > 0) {
+            if (!is_null($this->formHelperPrep)) {
+                $data = array_merge($data, $this->formHelperPrep);
+            }
+            $this->container['dispatcher']->dispatch('validation.failed', $this->container['standard_event']($violations));
+            $this->formHelper = $this->container['form_helper_post']($data, $violations);
+            
+            return call_user_func_array(array($this, $callback_method), $callback_params);
+        }
+    }
+
+    public function dispatch($event_name, $payload = null)
+    {
+        $standard_event = $this->container['standard_event']($payload);
+        $this->container['dispatcher']->dispatch($event_name, $standard_event);
+    }
+
+    protected function prepareFormHelper($object = null, $id_getter = 'getId', $id_field = 'id')
+    {
+        if (is_null($this->formHelper) && !is_null($object)) {
+            $this->formHelper = $this->container['form_helper_methods']($object);
+        }
+
+        if (!is_null($object)) {
+            $this->formHelperPrep = array($id_field => $object->$id_getter());
+        }
+
+        return $this->formHelper;
     }
 }
